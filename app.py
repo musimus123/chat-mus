4e92cb5540298bcfbc7df203ec398da2686d01ed
Edwin Mamani Rocha <musimusrocha@gmail.com>
2026-06-19 08:29:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -362,6 +362,7 @@ if __name__ == "__main__":
     socketio.run(
     app,
     host="0.0.0.0",
-    port=int(os.environ.get("PORT", 5000)),
-    debug=False
-    )
+    port=5000,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
